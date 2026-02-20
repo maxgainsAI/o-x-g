32afaf817c735eddf0a35a288dabcc859c32a2a1
maxgainsAI <maxgainsAI@icloud.com>
2026-02-20 04:05:47 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: 'black', color: '#00ffff', height: '100vh', fontFamily: "'Press Start 2P', monospace", display: 'flex', flexDirection: 'column', alignItems: '
